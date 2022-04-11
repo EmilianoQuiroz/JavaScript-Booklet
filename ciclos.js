fb71616862dee7394f3bb5f091c0c7826e2b80d9
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2022-04-11 06:34:16 -0300

    Ciclos e iteraciones

---
+++ ciclos.js
@@ -0,0 +1,7 @@
+//Ejercicio 1
+/*
+Solicitar al usuario un numero y un texto. Efectuar una
+salida por alerta con el mensaje ingresado por cada 
+repeticion, hasta alcanzar el calor señalado por el 
+usuario.
+*/
